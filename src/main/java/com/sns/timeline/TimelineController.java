5cdf747ad4d9e547e31883e3ec9128fbc24a0569
DESKTOP-L7G5VSR\user <test@test.com>
2024-07-17 15:53:25 +0900

    240716 타임라인 글 목록 뿌리기

---
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -0,0 +1,27 @@
+package com.sns.timeline;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+
+@Controller
+public class TimelineController {
+
+	@Autowired
+	private PostBO postBO;
+	
+	@GetMapping("/timeline/timeline-view")
+	public String timelineView(Model model) {
+		List<PostEntity> postList = postBO.getPostEntityList(); // json으로 풀기:getPostEntityList()
+		
+		model.addAttribute("postList", postList);
+		
+		return "timeline/timeline";
+	}
+}
